fe4f79d0ddf6612dd09145d6a0e49ed854c5e651
Eric Tillmann Bill <ericbill@Erics-MacBook-Air.local>
2023-05-17 22:43:14 +0200

    removed f1 score metric for sweep

--- run_script.sh
+++ run_script.sh
@@ -36,4 +36,4 @@ export PATH="$CONDA_ROOT/bin:$PATH"
 conda activate base
 
 ### Begin of executable commands
-python -m wandb wandb agent eric-bill/BachelorThesis/ao0fa3qm
+python -m wandb agent eric-bill/BachelorThesis/kmfi41t6
