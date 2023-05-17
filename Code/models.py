c92e8b8957a66a3f58e625093a4951be2b984217
Eric Tillmann Bill <ericbill@Erics-MacBook-Air.local>
2023-05-17 20:50:34 +0200

    ready for big run

--- Code/models.py
+++ Code/models.py
@@ -22,11 +22,48 @@ def load_model(model_name, output_dim, is_classification, device, largest_color 
                                 ]).to(device)
             
         else:
-            model = torch_geometric.nn.Sequential('x, edge_index, batch', [
-                                    (torch.nn.Embedding(num_embeddings=largest_color, embedding_dim=10), 'x -> x'),
-                                    (torch.squeeze, 'x -> x'),
-                                    (torch_geometric.nn.pool.global_add_pool, 'x, batch -> x'),
-                                    (MLP(channel_list=[10, 64, 32, 16, output_dim]), 'x -> x'),
-                                ]).to(device)
+            raise ValueError("1WL+NN:Embedding-Max is not implemented for regression tasks")
+
     
+    elif model_name == "1WL+NN:Embedding-Max":
+
+        if is_classification:
+
+            if largest_color is None:
+                raise ValueError("largest_color must be specified for classification tasks")
+            if embedding_dim is None:
+                raise ValueError("embedding_dim must be specified for classification tasks")
+
+            model = torch_geometric.nn.Sequential('x, edge_index, batch', [
+                                    (torch.nn.Embedding(num_embeddings=largest_color, embedding_dim=embedding_dim), 'x -> x'),
+                                    (torch.squeeze, 'x -> x'),
+                                    (torch_geometric.nn.pool.global_max_pool, 'x, batch -> x'),
+                                    (MLP(channel_list=[embedding_dim] + mlp_hidden_layer_conf + [output_dim]), 'x -> x'),
+                                    (torch.nn.Softmax(dim=1), 'x -> x')
+                                ]).to(device)
+            
+        else:
+            raise ValueError("1WL+NN:Embedding-Max is not implemented for regression tasks")
+    
+    elif model_name == "1WL+NN:Embedding-Mean":
+
+        if is_classification:
+
+            if largest_color is None:
+                raise ValueError("largest_color must be specified for classification tasks")
+            if embedding_dim is None:
+                raise ValueError("embedding_dim must be specified for classification tasks")
+
+            model = torch_geometric.nn.Sequential('x, edge_index, batch', [
+                                    (torch.nn.Embedding(num_embeddings=largest_color, embedding_dim=embedding_dim), 'x -> x'),
+                                    (torch.squeeze, 'x -> x'),
+                                    (torch_geometric.nn.pool.global_mean_pool, 'x, batch -> x'),
+                                    (MLP(channel_list=[embedding_dim] + mlp_hidden_layer_conf + [output_dim]), 'x -> x'),
+                                    (torch.nn.Softmax(dim=1), 'x -> x')
+                                ]).to(device)
+        
+        else:
+            raise ValueError("1WL+NN:Embedding-Max is not implemented for regression tasks")
+
+
     return model
